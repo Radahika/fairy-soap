e5913fb6fb006dec56aa4efa296d8326b969d6dc
Radahika <radhika.marvin@berkeley.edu>
2014-04-04 23:11:44 -0700

    Working form

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def chat_room():
 	return render_template('chat.html')
 
+@app.route("/test", methods=["POST"])
+def test():
+	return request.form["message_box"]
+
 if __name__ == "__main__": 	
+	app.debug = True
 	app.run()
 
