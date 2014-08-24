b596d474d422b2c5340bbb27536fa1d3a032dd68
Radahika <radhika.marvin@berkeley.edu>
2014-08-23 19:44:59 -0700

    Chat and Index original. About to do some testing

--- app/views.py
+++ app/views.py
@@ -1,15 +1,21 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, EditForm
-from models import User, ROLE_USER, ROLE_ADMIN
+from forms import LoginForm, EditForm, PostForm
+from models import User, ROLE_USER, ROLE_ADMIN, Post
 from datetime import datetime
+from config import POSTS_PER_PAGE
 
-@app.route('/')
-@app.route('/chat')
+@app.route('/', methods = ['GET', 'POST'])
+@app.route('/chat', methods = ['GET', 'POST'])
 @login_required
 def chat():
-    user = g.user
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body = form.post.data, timestamp = datetime.utcnow(), author = g.user)
+        db.session.add(post)
+        db.session.commit()
+        return redirect(url_for('chat'))
     posts = [ # fake array of posts
             {
                 'author': { 'nickname': 'Katie' },
@@ -21,9 +27,26 @@ def chat():
             }
         ]
     return render_template("chat.html",
-            title = 'Home',
+            title = 'Chat',
             user = user,
             posts = posts)
+
+@app.route('/index', methods = ['GET', 'POST'])
+@app.route('/index/<int:page>', methods = ['GET', 'POST'])
+@login_required
+def index(page = 1):
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body = form.post.data, timestamp = datetime.utcnow(), author = g.user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = g.user.followed_posts().paginate(page, POSTS_PER_PAGE, False)
+    return render_template('index.html',
+        title = 'Post',
+        form = form,
+        posts = posts)
 
 @app.route('/login', methods = ['GET', 'POST'])
 @oid.loginhandler
@@ -83,16 +106,14 @@ def logout():
     return redirect(url_for('chat'))
 
 @app.route('/user/<nickname>')
+@app.route('/user/<nickname>/<int:page>')
 @login_required
-def user(nickname):
+def user(nickname, page = 1):
     user = User.query.filter_by(nickname = nickname).first()
     if user == None:
         flash('User ' + nickname + ' not found.')
         return redirect(url_for('index'))
-    posts = [
-            { 'author': user, 'body': 'Test post #1' },
-            { 'author': user, 'body': 'Test post #2' }
-        ]
+    posts = user.posts.paginate(page, POSTS_PER_PAGE, False)
     return render_template('user.html',
         user = user,
         posts = posts)
